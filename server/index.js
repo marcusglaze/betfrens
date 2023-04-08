010d52cb9cf1e433c015cf136e2a8a3edeb94c1c
Marcus Glaze <marcusglz94@gmail.com>
2023-04-08 13:09:25 -0400

    Adding console log to index.js

--- server/index.js
+++ server/index.js
@@ -5,8 +5,10 @@ const app = express()
 
 // For testing with react native app -- can't release on app store?
 app.use('/apple-app-site-association', express.static(path.join(__dirname, 'apple-app-site-association')))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.port || 3000
 
-app.listen(PORT)
+app.listen(PORT, () => {
+    console.log("Listen on the port 3000...")
+})
